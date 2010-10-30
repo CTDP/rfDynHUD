18ac9249df4f33d1df259cac01a615f32d6b0e17
Marvin Fröhlich <Marvin.Froehlich@qudus.de>
2010-10-30 15:46:14 +0000

    Code refactoring.
    git-svn-id: https://svn.ctdp.net/svn/rfdynhud/trunk@405 1c0d6ce8-a1e3-b74c-a49d-f790e2421cd5

--- java/src/net/ctdp/rfdynhud/widgets/extra/assembled/AssembledWidget.java
+++ java/src/net/ctdp/rfdynhud/widgets/extra/assembled/AssembledWidget.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package net.ctdp.rfdynhud.widgets.extra;
+package net.ctdp.rfdynhud.widgets.extra.assembled;
 
 import net.ctdp.rfdynhud.widgets.base.widget.AbstractAssembledWidget;
 import net.ctdp.rfdynhud.widgets.base.widget.Widget;
