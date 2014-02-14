120a9afe6649b3c08100fbae7bc550231b677dd5
Marvin Fröhlich <Marvin.Froehlich@qudus.de>
2014-02-14 21:13:32 +0100

    test

---
+++ java/aaaa/test/UDPClient.java
@@ -0,0 +1,9 @@
+package test;
+
+public class UDPClient
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ java/aaaa/test/UDPServer.java
@@ -0,0 +1,9 @@
+package test;
+
+public class UDPServer
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
