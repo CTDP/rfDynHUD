90de6b79cf7192f2905d2937cf9a7bef6a825559
Marcel Offermans <marcel.offermans@luminis.nl>
2014-02-14 21:21:57 +0100

    Client that sends a single packet.

--- java/aaaa/test/UDPClient.java
+++ java/aaaa/test/UDPClient.java
@@ -1,9 +1,22 @@
 package test;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class UDPClient
 {
-    public static void main( String[] args )
+	// sends a single packet with some 'random' data
+    public static void main( String[] args ) throws Exception
     {
-        
+    	int port = 4445;
+    	DatagramSocket socket = null;
+    	byte[] buf = new byte[508];
+    	for (int i = 0; i < buf.length; i++) {
+    		buf[i] = (byte)(i % 64);
+    	}
+    	InetAddress address = InetAddress.getByName(args.length > 0 ? args[0] : "localhost");
+    	DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+    	socket.send(packet);
     }
 }
