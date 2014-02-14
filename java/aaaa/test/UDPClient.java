b1c036ea1607d0e7ebdd549e34d39cbdcf700549
Marcel Offermans <marcel.offermans@luminis.nl>
2014-02-14 21:35:03 +0100

    Can send arbitrary length data (still no headers in packets though).

--- java/aaaa/test/UDPClient.java
+++ java/aaaa/test/UDPClient.java
@@ -1,22 +1,34 @@
 package test;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPClient
 {
+	private static InetAddress address;
+	private static int port;
+	private static DatagramSocket socket;
+
 	// sends a single packet with some 'random' data
     public static void main( String[] args ) throws Exception
     {
-    	int port = 4445;
-    	DatagramSocket socket = null;
+    	address = InetAddress.getByName(args.length > 0 ? args[0] : "localhost");
+    	port = 4445;
+		socket = new DatagramSocket();
+		System.out.println("Sending data...");
+		send("This is a test".getBytes());
+		System.out.println("Done.");
+    }
+    
+    public static void send(byte[] data) throws IOException {
     	byte[] buf = new byte[508];
-    	for (int i = 0; i < buf.length; i++) {
-    		buf[i] = (byte)(i % 64);
+    	for (int i = 0; i < data.length; i += 508) {
+    		int length = data.length - i;
+    		System.arraycopy(data, i, buf, 0, length > 508 ? 508 : length);
+    		DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+    		socket.send(packet);
     	}
-    	InetAddress address = InetAddress.getByName(args.length > 0 ? args[0] : "localhost");
-    	DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
-    	socket.send(packet);
     }
 }
